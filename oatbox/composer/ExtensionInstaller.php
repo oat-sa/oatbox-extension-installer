db18694a1a9127f790c659384e4c19232d467a1d
Mikhail Kamarouski <Komarouski@1pt.com>
2015-11-23 11:42:58 +0300

    Support repositories with type 'path'

--- oatbox/composer/ExtensionInstaller.php
+++ oatbox/composer/ExtensionInstaller.php
@@ -13,7 +13,22 @@ class ExtensionInstaller extends LibraryInstaller
         $ext = self::EXTENSION_NAME_KEY;
         $extra = $package->getExtra();
         if(!is_null($extra) && isset($extra[$ext])){
-            return $extra[$ext];
+            $baseDir = '';
+            try {
+                $reader = function ($object, $property) {
+                    $value =  \Closure::bind(function & () use ($property) {
+                        return $this->$property;
+                    }, $object, $object)->__invoke();
+
+                    return $value;
+                };
+                $config = $reader($this->downloadManager->getDownloader('path'), 'config');
+                $baseDir = $reader($config, 'baseDir');
+                $baseDir = realpath($baseDir).DIRECTORY_SEPARATOR;
+            } catch (\InvalidArgumentException $e) {
+
+            }
+            return $baseDir . $extra[$ext];
         } else {
             throw new \InvalidArgumentException('could not find extension name in manifest');
         }
