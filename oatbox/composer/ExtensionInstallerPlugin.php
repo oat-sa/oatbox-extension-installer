8175324fa57e37616528822bdc3b23ae0776cb57
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2020-04-14 14:52:54 +0300

    Plugin adjusted to met a new requirements from Composer 2.0

--- oatbox/composer/ExtensionInstallerPlugin.php
+++ oatbox/composer/ExtensionInstallerPlugin.php
@@ -6,12 +6,27 @@ use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
+/**
+ * Class ExtensionInstallerPlugin
+ * @package oatbox\composer
+ */
 class ExtensionInstallerPlugin implements PluginInterface
 {
-    public function activate(Composer $composer, IOInterface $io){
-        $installer = new ExtensionInstaller($io, $composer);
-        $composer->getInstallationManager()->addInstaller($installer);
+    private $installer;
+
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $this->installer = new ExtensionInstaller($io, $composer);
+
+        $composer->getInstallationManager()->addInstaller($this->installer);
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        $composer->getInstallationManager()->removeInstaller($this->installer);
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
     }
 }
-
-?>
