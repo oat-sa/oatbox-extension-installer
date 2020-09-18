e3b7da0afcdc50231ef5bf58b7ccc149190c1999
gitromba <mihaly.hajdu@taotesting.com>
2020-09-18 13:35:03 +0200

    feat: turn off plugin

--- oatbox/composer/ExtensionInstallerPlugin.php
+++ oatbox/composer/ExtensionInstallerPlugin.php
@@ -9,8 +9,8 @@ use Composer\Plugin\PluginInterface;
 class ExtensionInstallerPlugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io){
-        $installer = new ExtensionInstaller($io, $composer);
-        $composer->getInstallationManager()->addInstaller($installer);
+//        $installer = new ExtensionInstaller($io, $composer);
+//        $composer->getInstallationManager()->addInstaller($installer);
     }
 }
 
